2e26503751f6cd3692331ab1cd7a4916b336617d
Antonio Jesus <estopero88@gmail.com>
2019-01-25 19:42:43 +0100

    testing camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,11 +9,20 @@ public class CameraController : MonoBehaviour
     [Tooltip("The pivot forward will set the camera forward")]
     Transform _pivotPoint;
 
+    [SerializeField]
     [Header("Min Angle Allow")]
-    float _minAngle;
+    Vector2 _minAngle;
 
+    [SerializeField]
     [Header("Max Angle Allow")]
-    float _maxAngle;
+    Vector2 _maxAngle;
+
+    [SerializeField]
+    [Header("Camera Speed")]
+    float _speed;
+
+    Vector3 _currentRotation;
+
 
     void Start()
     {
@@ -22,7 +31,29 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        
+        //transform.RotateAround(_pivotPoint.position, Vector3.up, _speed * Time.deltaTime);
+        transform.RotateAround(_pivotPoint.position, Vector3.right, 0.5f * _speed * Time.deltaTime);
+
+        CheckAngleLimits();
+    }
+
+    void CheckAngleLimits()
+    {
+        _currentRotation = transform.eulerAngles;
+
+        // rotations around X angle
+        if (_currentRotation.x <= _minAngle.y)
+            _currentRotation.Set(_minAngle.y, _currentRotation.y, _currentRotation.z);
+        else if (_currentRotation.x >= _maxAngle.y)
+            _currentRotation.Set(_maxAngle.y, _currentRotation.y, _currentRotation.z);
+
+        // rotations around X angle
+        if (_currentRotation.y <= _minAngle.x)
+            _currentRotation.Set(_currentRotation.x, _minAngle.x, _currentRotation.z);
+        else if (_currentRotation.y >= _maxAngle.x)
+            _currentRotation.Set(_currentRotation.x, _maxAngle.x, _currentRotation.z);
+
+        transform.eulerAngles = _currentRotation;
     }
 
     private void OnDrawGizmos()
---
+++ Assets/Scripts/TouchWrapper/TouchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : SingletonGameObject<TouchController>
+{
+    
+
+    public struct TouchWrapper
+    {
+        Vector2 position;
+        Vector2 deltaPosition;
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IOS)
+        GetInput()
+#else 
+        GetMouse();
+#endif
+    }
+
+    private void GetMouse()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
