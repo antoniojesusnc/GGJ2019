8c1bbba60cc5723dd4294297331c4658414ab534
Antonio Jesus <estopero88@gmail.com>
2019-01-25 19:01:38 +0100

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Camera _camera;
+
+    [SerializeField]
+    [Header("Pivot point for the camera")]\
+    [Tooltip("The pivot forward will set the camera forward")]
+    Transform _pivotPoint;
+
+    [Header("Min Angle Allow")]
+    float _minAngle;
+
+    [Header("Max Angle Allow")]
+    float _maxAngle;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+
+    }
+}
