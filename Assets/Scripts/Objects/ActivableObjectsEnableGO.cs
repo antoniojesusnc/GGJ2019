0692a950728f6e1528279fe6462aeeb7cf16dad6
Antonio Jesus <estopero88@gmail.com>
2019-01-26 23:38:11 +0100

    changes in lv02 mesh

--- Assets/Scripts/Objects/ActivableObjectsEnableGO.cs
+++ Assets/Scripts/Objects/ActivableObjectsEnableGO.cs
@@ -6,7 +6,7 @@ public class ActivableObjectsEnableGO : ActivableObjects
 {
     protected override IEnumerator MoveToNextStateCo(Vector3 position, Quaternion rotation, ObjectState newState)
     {
-        _secondPosAndRot.gameObject.SetActive(newState == ObjectState.Pos1);
+        _secondPosAndRot.gameObject.SetActive(newState == ObjectState.Pos2);
 
         _currentState = newState;
         yield return 0;
